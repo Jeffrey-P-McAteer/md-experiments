da85ac3314f791be412afeb05e0eba9512358aba
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-01-01 15:03:23 -0500

    deps

--- src/Program.cs
+++ src/Program.cs
@@ -1,15 +1,75 @@
 using System;
- using Microsoft.CodeAnalysis.CSharp.Scripting;
 
-namespace ConsoleApplication
-{
-    public class Program
-    {
-        public static void Main()
-        {
-            var result = CSharpScript.EvaluateAsync("1 + 3").Result;
-            Console.WriteLine("Hello World! result = "+result);
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace ConsoleApplication {
+    public class Program {
+        public static void Main() {
+
+
+          var t0_data = Data.all();
+          var tn_data = Data.all();
+
+          var deltas = Delta.all();
+          var conditions = Condition.all();
+
+          var t0 = DateTime.Now.AddDays(-10);
+          var tf = DateTime.Now;
+
+
+
+          var result = CSharpScript.EvaluateAsync("1 + 3").Result;
+          Console.WriteLine("Hello World! result = "+result);
+
         }
+    }
+
+    public class Data {
+      public int oid;
+
+      public double x;
+      public double y;
+
+      public string name;
+      public string color;
+      public string status;
+
+      public static List<Data> all() {
+        return new List<Data>(){
+          new Data(){oid=1, x=0.0, y=0.0, name="a", color="red", status=""},
+          new Data(){oid=1, x=1.0, y=0.0, name="b", color="green", status=""},
+          new Data(){oid=1, x=1.5, y=0.5, name="b", color="red", status=""},
+        };
+      }
+    }
+
+    public class Delta {
+
+      public string name;
+      public string description;
+
+
+      public static List<Delta> all() {
+        return new List<Delta>(){
+          new Delta(){
+            name="",
+            description="",
+
+          }
+
+        };
+      }
+    }
+    public class Condition {
+      public static List<Condition> all() {
+        return new List<Condition>(){
+
+
+        };
+      }
     }
 }
 
